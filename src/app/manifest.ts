2f2b048916a486c7f6bb0241c6994ddc8fd0537e
Seungpyo Suh <me@sspzoa.io>
2025-04-10 20:39:28 +0900

    favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,33 +8,58 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     display: 'fullscreen',
     orientation: 'portrait-primary',
+    theme_color: '#000000',
+    background_color: '#ffffff',
     icons: [
       {
-        src: '/favicon.ico',
-        type: 'image/x-icon',
+        src: './favicon.ico',
         sizes: '16x16 32x32',
+        type: 'image/x-icon',
       },
       {
-        src: '/logo/icon-192.png',
-        sizes: '192x192',
+        src: './logo/favicon-32x32.png',
+        sizes: '32x32',
         type: 'image/png',
       },
       {
-        src: './logo/icon-512.png',
+        src: './logo/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
       {
-        src: '/logo/icon-192-maskable.png',
-        sizes: '192x192',
+        src: './logo/apple-icon.png',
+        sizes: '180x180',
         type: 'image/png',
-        purpose: 'maskable',
       },
       {
-        src: './logo/icon-512-maskable.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
+        src: './logo/android-icon-36x36.png',
+        sizes: '36x36',
+        type: 'image/png'
+      },
+      {
+        src: './logo/android-icon-48x48.png',
+        sizes: '48x48',
+        type: 'image/png'
+      },
+      {
+        src: './logo/android-icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png'
+      },
+      {
+        src: './logo/android-icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png'
+      },
+      {
+        src: './logo/android-icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png'
+      },
+      {
+        src: './logo/android-icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
       },
     ],
   }
