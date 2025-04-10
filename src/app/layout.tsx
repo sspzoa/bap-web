ab3eca7b67a2498dac758df8a31814d9c875dfed
Seungpyo Suh <me@sspzoa.io>
2025-04-10 10:59:42 +0900

    revert: remove font changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,6 @@ const suitFont = localFont({
   ],
   variable: '--font-suit',
   display: 'swap',
-  preload: true,
-  fallback: ['sans-serif'],
 });
 
 export const metadata: Metadata = {
@@ -27,11 +25,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="ko" className={`${suitFont.variable}`}>
+    <html lang="ko">
     <head>
       <meta name="google-site-verification" content="Autqjgf5q34Q-Bi4JnRwIuiJW-WzwkCU6Y4wlGU0IVU" />
     </head>
-    <body className={`antialiased font-suit`}>
+    <body className={`antialiased`}>
     <Providers>
       {children}
     </Providers>
