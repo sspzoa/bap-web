264e36bb806de4885ac1b753f57025c7ad6298b2
Seungpyo Suh <me@sspzoa.io>
2025-06-28 20:22:08 +0900

    chore: update Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,30 @@
 import type { NextConfig } from 'next';
+import { NextResponse } from 'next/server';
 
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // 모든 경로에 대해 적용
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
   },
 };
 
