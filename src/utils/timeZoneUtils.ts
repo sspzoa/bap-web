758fcbfe1cb053c7e339a9102b831e18a7f2b81e
Seungpyo Suh <me@sspzoa.io>
2025-06-30 19:08:06 +0900

    tz2

--- src/utils/timeZoneUtils.ts
+++ src/utils/timeZoneUtils.ts
@@ -1,5 +1,13 @@
 import { format } from 'date-fns';
+import { toZonedTime } from 'date-fns-tz';
 
 export const formatToDateString = (date: Date): string => {
   return format(date, 'yyyy-MM-dd');
+};
+
+const KOREA_TIMEZONE = 'Asia/Seoul';
+
+export const getKoreanHours = (): number => {
+  const koreanTime = toZonedTime(new Date(), KOREA_TIMEZONE);
+  return koreanTime.getHours();
 };
