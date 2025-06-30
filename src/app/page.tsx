ebc35f718799561b195ef865dc44d99e629f2615
Seungpyo Suh <me@sspzoa.io>
2025-06-30 21:50:58 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import MealLayout from '@/components/MealLayout';
 import { getMealDataServerSide } from '@/services/mealService';
 import { getInitialDateForServer } from '@/utils/dateUtils';
-import { formatToDateString } from '@/utils/timeZoneUtils';
+import { formatToDateString, getKoreanTime } from '@/utils/timeZoneUtils';
 
 export default async function Page() {
+  const koreanTime = getKoreanTime();
   const initialDate = getInitialDateForServer();
   const formattedDate = formatToDateString(initialDate);
-  const initialData = await getMealDataServerSide(formattedDate);
-  const serverDate = new Date();
 
-  console.log(`Fetching data for date: ${formattedDate}, ${serverDate}`);
+  const initialData = await getMealDataServerSide(formattedDate);
+
+  console.log(`Server Korean Time: ${koreanTime.toISOString()}`);
+  console.log(`Fetching data for Korean date: ${formattedDate}`);
+  console.log(`Initial Date: ${initialDate.toISOString()}`);
 
   return <MealLayout initialData={initialData} initialDate={initialDate} />;
 }
