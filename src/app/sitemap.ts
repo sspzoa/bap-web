54e09c923fb5f5744ed0c39f9736d7f01b19df0c
Seungpyo Suh <me@sspzoa.io>
2025-07-17 10:11:16 +0900

    robot.txt

---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://밥.net',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ];
+}
