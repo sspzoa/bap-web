c976cbeb43f2f485e422845934bad7b9db567985
Seungpyo Suh <me@sspzoa.io>
2025-09-18 14:28:25 +0900

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    display: 'standalone',
+    display: 'fullscreen',
     orientation: 'natural',
     icons: [
       {
