d54f14d33015737c5e55bc881b166d204351e743
Seungpyo Suh <me@sspzoa.io>
2025-09-22 13:28:59 +0900

    chore: glass blur

--- src/components/ui/Glass.tsx
+++ src/components/ui/Glass.tsx
@@ -9,7 +9,7 @@ interface GlassProps extends HTMLAttributes<HTMLDivElement> {
 export default memo(function Glass({ children, className = '', ...props }: GlassProps) {
   return (
     <div
-      className={`rounded-[15px] bg-white/20 border-2 border-white/10 backdrop-blur-[48px] shadow-[0_0_10px_rgba(0,0,0,0.05)] overflow-y-auto ${className}`}
+      className={`rounded-[15px] bg-white/20 border-2 border-white/10 backdrop-blur-[24px] shadow-[0_0_10px_rgba(0,0,0,0.05)] overflow-y-auto ${className}`}
       {...props}>
       {children}
     </div>
